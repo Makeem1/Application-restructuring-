0d163a459b0efd8a676545315bff6cb1f19a96e1
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-03-17 11:55:42 -0400

    add error505 route and page

--- snakeeyes/blueprints/error_page/views.py
+++ snakeeyes/blueprints/error_page/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 def page_not_found(e):
     return render_template('error/error_404.html'), 404
 
+@error.app_errorhandler(500)
+def ser_error(e):
+    return render_template('error/error_500.html'), 500
+    
