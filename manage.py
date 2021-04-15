4a583a9f0018b7d93a496b03ce07e21ba4243223
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-04-15 23:36:33 +0100

    adding coupon, subscription , credit_card and invoice class

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@ from snakeeyes import create_app
 from flask_script import Manager, Shell
 from snakeeyes.blueprints.user.models import User
 from snakeeyes.extensions import db
+from snakeeyes.blueprints.billing.models.coupon import Coupon
+from snakeeyes.blueprints.billing.models.invoice import Invoice
+from snakeeyes.blueprints.billing.models.subscriptions import Subscription
+from snakeeyes.blueprints.billing.models.credit_card import CreditCard
 
 from stripe_utils import create_plan, delete_plan, list_plan
 
@@ -11,7 +15,7 @@ manager = Manager(app)
 db.app = app 
 
 def make_shell_context():
-    return dict(app=app, User=User, db=db, create_plan=create_plan, delete_plan=delete_plan, list_plan = list_plan )
+    return dict(app=app, User=User, Coupon=Coupon, Invoice=Invoice, Subscription=Subscription, CreditCard=CreditCard, db=db, create_plan=create_plan, delete_plan=delete_plan, list_plan = list_plan )
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
