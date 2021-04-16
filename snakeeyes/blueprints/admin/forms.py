afb1c242a10c5409be20263e5ddcd03760589a7f
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-04-16 17:29:45 +0100

    formating import

--- snakeeyes/blueprints/admin/forms.py
+++ snakeeyes/blueprints/admin/forms.py
@@ -1,7 +1,26 @@
 from flask_wtf import Form
-from wtforms import StringField, SelectField, ValidationError, BooleanField, SelectField
-from wtforms.validators import DataRequired, Optional, Length
+from wtforms import (
+    StringField, 
+    SelectField,
+    ValidationError, 
+    BooleanField, 
+    SelectField, 
+    IntegerField,
+    FloatField, 
+    DateTimeField
+    )
+
+from wtforms.validators import (
+    DataRequired, 
+    Optional, 
+    Length,
+    NumberRange
+    )
+
 from snakeeyes.blueprints.user.models import User
+from lib.locale import Currency
+from lib.utils_wtform import choices_from_dict
+from snakeeyes.blueprints.billing.models.coupon import Coupon
 
 
 class SearchForm(Form):
@@ -33,3 +52,57 @@ class UserForm(Form):
 
 class CanUserSubscriptionForm(Form):
     pass
+
+
+class CouponForm(Form):
+    percent_off = IntegerField('Percent of (%)',
+                                validators=[Optional(), NumberRange(min=1, max=100)])
+    amount_off = FloatField('Amount off ($)', 
+                                validators=[Optional(), NumberRange(min=0.01, max=21474836.0)])
+    
+    code = StringField('Code', 
+                            validators=[DataRequired(), Length(min=1, max=32)])
+    
+    currency = SelectField('Currency', validators=[DataRequired()], 
+                            choices=choices_from_dict(Currency.TYPES, prepend_blank=False))
+    
+    duration = SelectField('Duaration', 
+                            validators=[DataRequired()], 
+                            choices=choices_from_dict(Coupon.DURATION, prepend_blank=False))
+    
+    duration_in_months = IntegerField('Duration', 
+                        validators=[Optional(), NumberRange(min=1, max=12) ])
+    
+    max_redemptions = IntegerField('Max Redeemptions', 
+                                    validators=[Optional(), NumberRange(min=1, max=2147483647)])
+    
+    redeem_by = DateTimeField('Redeem by', 
+                                validators=[Optional()], format='%Y-%m-%d %H:%M:%S')
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+
+        result = True
+
+        percent_off = self.percent_off.data
+        amount_off = self.amount_off.data
+
+        if percent_off is None and amount_off is None:
+            empty_error = 'Pick atleast one.'
+            self.percent_off.errors.append(empty_error)
+            self.amount_off.errors.append(empty_error)
+            result = False
+        elif percent_off and amount_off:
+            both_error = 'Cannot pick both'
+            self.percent_off.errors.append(both_error)
+            self.amount_off.errors.append(both_error)
+
+            return False
+        else:
+            pass
+
+
+        return result
+
+
