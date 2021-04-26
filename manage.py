47e2bf09d2670d2b9b3ffc43be96455fbe616720
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-04-26 23:33:14 +0100

    adding Bet to make_shell_context function

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ from snakeeyes.blueprints.billing.models.coupon import Coupon
 from snakeeyes.blueprints.billing.models.invoice import Invoice
 from snakeeyes.blueprints.billing.models.subscriptions import Subscription
 from snakeeyes.blueprints.billing.models.credit_card import CreditCard
+from snakeeyes.blueprints.bet.models.bet import Bet
 
 from stripe_utils import create_plan, delete_plan, list_plan
 
@@ -15,7 +16,7 @@ manager = Manager(app)
 db.app = app 
 
 def make_shell_context():
-    return dict(app=app, User=User, Coupon=Coupon, Invoice=Invoice, Subscription=Subscription, CreditCard=CreditCard, db=db, create_plan=create_plan, delete_plan=delete_plan, list_plan = list_plan )
+    return dict(app=app, User=User, Coupon=Coupon, Invoice=Invoice, Subscription=Subscription, CreditCard=CreditCard, Bet=Bet, db=db, create_plan=create_plan, delete_plan=delete_plan, list_plan = list_plan )
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
